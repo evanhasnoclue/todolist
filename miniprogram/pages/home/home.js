b38219717c69cc8aef644debb86e17d87ae74401
evanhasnoclue <wangkai199385@126.com>
2021-02-24 17:57:11 +0800

    home page header design

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,18 +1,30 @@
 // pages/home.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else {
+      wx.redirectTo({
+        url: '../index/index',
+      })
+    }
   },
 
   /**
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -20,6 +20,9 @@ Page({
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
+      });
+      wx.redirectTo({
+        url: '../home/home',
       })
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
@@ -28,6 +31,9 @@ Page({
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
+        });
+        wx.redirectTo({
+          url: '../home/home',
         })
       }
     } else {
@@ -38,6 +44,9 @@ Page({
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
+          });
+          wx.redirectTo({
+            url: '../home/home',
           })
         }
       })
@@ -49,6 +58,9 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    });
+    wx.redirectTo({
+      url: '../home/home',
     })
   }
 })
